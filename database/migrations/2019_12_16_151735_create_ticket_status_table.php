89de5eb88fcb7fb0efd53da130e68bb16dbf386e
Julio Muller <juliodolago@gmail.com>
2019-12-21 15:18:58 -0300

    Renamed file/class to match table name

--- database/migrations/2019_12_16_151735_create_ticket_status_table.php
+++ database/migrations/2019_12_16_151735_create_ticket_status_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTicketStatusesTable extends Migration
+class CreateTicketStatusTable extends Migration
 {
     public function up()
     {
@@ -18,6 +18,6 @@ class CreateTicketStatusesTable extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('ticket_statuses');
+        Schema::dropIfExists('ticket_status');
     }
 }
