c9d917e75556946e0c75343142da79f4c5dd0efa
Julio Muller <juliodolago@gmail.com>
2019-12-20 23:28:43 -0300

    Added soft deletes to some model classes

--- app/Model/TicketMessage.php
+++ app/Model/TicketMessage.php
@@ -3,8 +3,9 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TicketMessage extends Model
 {
-    //
+    use SoftDeletes;
 }
--- app/Model/TicketStatus.php
+++ app/Model/TicketStatus.php
@@ -3,8 +3,16 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TicketStatus extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * Table associated with the model
+     *
+     * @var string
+     */
+    protected $table = 'ticket_status';
 }
