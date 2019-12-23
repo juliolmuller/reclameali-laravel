bfb710144b9778b64966796a146889cdea36e142
Julio Muller <juliodolago@gmail.com>
2019-12-23 00:03:28 -0300

    Configured providers for 'Faker'

---
+++ app/Providers/FakerServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Bezhanov\Faker\ProviderCollectionHelper;
+use Faker\Factory;
+use Faker\Generator;
+use Illuminate\Support\ServiceProvider;
+use JansenFelipe\FakerBR\FakerBR;
+
+class FakerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Generator::class, function () {
+            $faker = Factory::create();
+            ProviderCollectionHelper::addAllProvidersTo($faker);
+            $faker->addProvider(new FakerBR($faker));
+            return $faker;
+        });
+    }
+}
