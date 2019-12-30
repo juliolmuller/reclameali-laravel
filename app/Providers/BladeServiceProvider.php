43c0d96c63a17e66396fc1dbab2aefaf80c42802
Julio Muller <juliodolago@gmail.com>
2019-12-29 23:24:25 -0300

    Configured blade to use header and footer aliases

---
+++ app/Providers/BladeServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class BladeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::component('layout.header', 'header');
+        Blade::component('layout.footer', 'footer');
+    }
+}
