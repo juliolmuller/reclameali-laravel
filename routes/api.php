45e49eb94aaa1c55827f1870ef50102d774ceab8
Julio Muller <juliodolago@gmail.com>
2020-01-01 22:44:20 -0300

    Added routes for Categories API

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('/categories')->group(function () {
+    Route::get('/', 'CategoryController@index')->name('categories.index');
+    Route::post('/', 'CategoryController@store')->name('categories.store');
+    Route::get('/{category}', 'CategoryController@show')->name('categories.show');
+    Route::put('/{category}', 'CategoryController@update')->name('categories.update');
+    Route::delete('/{category}', 'CategoryController@destroy')->name('categories.destroy');
 });
