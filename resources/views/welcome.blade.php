07bdfb34f0ec325d5756ea0d8ade4a43cee0de39
Julio Muller <juliodolago@gmail.com>
2020-01-04 00:18:02 -0300

    Added Auth resources

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>
+      Laravel
+    </title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" />
     <style>
       html, body {
         background-color: #fff;
@@ -71,14 +68,12 @@
             <a href="{{ url('/home') }}">Home</a>
           @else
             <a href="{{ route('login') }}">Login</a>
-
             @if (Route::has('register'))
               <a href="{{ route('register') }}">Register</a>
             @endif
           @endauth
         </div>
       @endif
-
       <div class="content">
         <div class="title m-b-md">
           Laravel
