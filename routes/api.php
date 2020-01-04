cbd4838549dcaff8a31b9e7334504567b322e5a7
Julio Muller <juliodolago@gmail.com>
2020-01-03 21:51:13 -0300

    Added routes to Products API

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
-
 Route::prefix('/categories')->group(function () {
     Route::get('/', 'CategoryController@index')->name('categories.index');
     Route::post('/', 'CategoryController@store')->name('categories.store');
     Route::get('/{category}', 'CategoryController@show')->name('categories.show');
     Route::put('/{category}', 'CategoryController@update')->name('categories.update');
     Route::delete('/{category}', 'CategoryController@destroy')->name('categories.destroy');
+});
+
+Route::prefix('/products')->group(function () {
+    Route::get('/', 'ProductController@index')->name('products.index');
+    Route::post('/', 'ProductController@store')->name('products.store');
+    Route::get('/{product}', 'ProductController@show')->name('products.show');
+    Route::put('/{product}', 'ProductController@update')->name('products.update');
+    Route::delete('/{product}', 'ProductController@destroy')->name('products.destroy');
 });
