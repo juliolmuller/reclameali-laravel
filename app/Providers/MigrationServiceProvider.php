f4f7d33556c2c7e34458750c2ba1cf4d78e961a3
Julio Muller <juliodolago@gmail.com>
2020-01-03 21:03:13 -0300

    Added param to change default columns names

--- app/Providers/MigrationServiceProvider.php
+++ app/Providers/MigrationServiceProvider.php
@@ -15,19 +15,26 @@ class MigrationServiceProvider extends ServiceProvider
     public function boot()
     {
         Blueprint::macro('changesTracking', function (
+            array $fieldsName = [],
             string $usersTable = 'users',
             string $usersPrimary = 'id',
             string $fieldType = 'unsignedBigInteger'
         ) {
-            $this->timestamp('created_at')->nullable();
-            $this->$fieldType('created_by')->nullable();
-            $this->timestamp('updated_at')->nullable();
-            $this->$fieldType('updated_by')->nullable();
-            $this->timestamp('deleted_at')->nullable();
-            $this->$fieldType('deleted_by')->nullable();
-            $this->foreign('created_by')->references($usersPrimary)->on($usersTable);
-            $this->foreign('updated_by')->references($usersPrimary)->on($usersTable);
-            $this->foreign('deleted_by')->references($usersPrimary)->on($usersTable);
+            $fieldsName['created_at'] ??= 'created_at';
+            $fieldsName['updated_at'] ??= 'updated_at';
+            $fieldsName['deleted_at'] ??= 'deleted_at';
+            $fieldsName['created_by'] ??= 'created_by';
+            $fieldsName['updated_by'] ??= 'updated_by';
+            $fieldsName['deleted_by'] ??= 'deleted_by';
+            $this->timestamp($fieldsName['created_at'])->nullable();
+            $this->$fieldType($fieldsName['created_by'])->nullable();
+            $this->timestamp($fieldsName['updated_at'])->nullable();
+            $this->$fieldType($fieldsName['updated_by'])->nullable();
+            $this->timestamp($fieldsName['deleted_at'])->nullable();
+            $this->$fieldType($fieldsName['deleted_by'])->nullable();
+            $this->foreign($fieldsName['created_by'])->references($usersPrimary)->on($usersTable);
+            $this->foreign($fieldsName['updated_by'])->references($usersPrimary)->on($usersTable);
+            $this->foreign($fieldsName['deleted_by'])->references($usersPrimary)->on($usersTable);
         });
     }
 }
