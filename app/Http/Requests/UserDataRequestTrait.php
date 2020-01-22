a05557f31bb414150c7b557e8ea1e834e5ae0301
Julio Muller <juliodolago@gmail.com>
2020-01-22 00:00:16 -0300

    Added validation rules to UserDataRequestTrait

--- app/Http/Requests/UserDataRequestTrait.php
+++ app/Http/Requests/UserDataRequestTrait.php
@@ -13,15 +13,15 @@ trait UserDataRequestTrait
     {
         $exception = $this->isMethod('POST') ? '' : ",{$this->user->id}";
         return [
-            'first_name'    => ['bail', 'required', 'string', 'regex:/^[A-Za-zÀ-ÖØ-öø-ÿ ]+/', 'between:1,30'],
-            'last_name'     => ['bail', 'required', 'string', 'regex:/^[A-Za-zÀ-ÖØ-öø-ÿ ]+/', 'between:1,150'],
+            'first_name'    => ['bail', 'required', 'string', 'between:1,30', 'regex:/^[A-Za-zÀ-ÖØ-öø-ÿ\- ]+$/'],
+            'last_name'     => ['bail', 'required', 'string', 'between:1,150', 'regex:/^[A-Za-zÀ-ÖØ-öø-ÿ\- ]+$/'],
             'cpf'           => ['bail', 'required', 'integer', 'cpf', 'digits:11', "unique:users,cpf{$exception}"],
             'date_of_birth' => ['bail', 'required', 'before_or_equal:-18 years'],
             'email'         => ['bail', 'required', 'email', "unique:users,email{$exception}"],
-            'phone'         => ['bail', 'nullable', 'digits_between:10,11'],
+            'phone'         => ['bail', 'nullable', 'integer', 'digits_between:10,11'],
             'zip_code'      => ['bail', 'nullable', 'integer', 'digits:8'],
             'street'        => ['bail', 'nullable', 'string', 'max:255'],
-            'number'        => ['bail', 'nullable', 'integer'],
+            'number'        => ['bail', 'nullable', 'integer', 'min:0'],
             'complement'    => ['bail', 'nullable', 'string', 'max:20'],
             'state'         => ['bail', 'nullable', 'exists:states,id'],
             'city'          => ['bail', 'nullable', 'exists:cities,id'],
