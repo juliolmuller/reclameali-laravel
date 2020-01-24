05854f41bcc0db52e34a025b91a3e8e0462ea7e9
Julio Muller <juliodolago@gmail.com>
2020-01-24 08:08:42 -0300

    Removed 'integer' validation and set regexes instead

--- app/Http/Requests/UserDataRequestTrait.php
+++ app/Http/Requests/UserDataRequestTrait.php
@@ -11,15 +11,17 @@ trait UserDataRequestTrait
      */
     public function rules()
     {
+        $nameRegex = '/^[A-Za-zÀ-ÖØ-öø-ÿ\- ]+$/';
+        $numericRegex = '/^[0-9]+$/';
         $exception = $this->isMethod('POST') ? '' : ",{$this->user->id}";
         return [
-            'first_name'    => ['bail', 'required', 'string', 'between:1,30', 'regex:/^[A-Za-zÀ-ÖØ-öø-ÿ\- ]+$/'],
-            'last_name'     => ['bail', 'required', 'string', 'between:1,150', 'regex:/^[A-Za-zÀ-ÖØ-öø-ÿ\- ]+$/'],
-            'cpf'           => ['bail', 'required', 'integer', 'cpf', 'digits:11', "unique:users,cpf{$exception}"],
+            'first_name'    => ['bail', 'required', 'string', 'between:1,30', "regex:{$nameRegex}"],
+            'last_name'     => ['bail', 'required', 'string', 'between:1,150', "regex:{$nameRegex}"],
+            'cpf'           => ['bail', 'required', "regex:{$numericRegex}", 'cpf', 'digits:11', "unique:users,cpf{$exception}"],
             'date_of_birth' => ['bail', 'required', 'before_or_equal:-18 years'],
             'email'         => ['bail', 'required', 'email', "unique:users,email{$exception}"],
-            'phone'         => ['bail', 'nullable', 'integer', 'digits_between:10,11'],
-            'zip_code'      => ['bail', 'nullable', 'integer', 'digits:8'],
+            'phone'         => ['bail', 'nullable', "regex:{$numericRegex}", 'digits_between:10,11'],
+            'zip_code'      => ['bail', 'nullable', "regex:{$numericRegex}", 'digits:8'],
             'street'        => ['bail', 'nullable', 'string', 'max:255'],
             'number'        => ['bail', 'nullable', 'integer', 'min:0'],
             'complement'    => ['bail', 'nullable', 'string', 'max:20'],
