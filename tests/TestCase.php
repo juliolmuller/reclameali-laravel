041d19fa48e0b2542daca293bdf4679354d7b219
Julio Muller <juliodolago@gmail.com>
2020-01-25 19:48:15 -0300

    Added method to log data within tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,9 +5,25 @@ namespace Tests;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Log;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication,
         DatabaseTransactions;
+
+    protected function log($driver, $stack = [])
+    {
+        $message = $driver;
+
+        if (count($stack)) {
+            $message .= ":\n";
+
+            foreach ($stack as $key => $value) {
+                $message .= "\t{$key} => {$value}\n";
+            }
+        }
+
+        Log::debug($message);
+    }
 }
