af2e1a81286d8dea60323e4784275fd931c77b6d
Julio Muller <juliodolago@gmail.com>
2020-01-28 08:34:04 -0300

    Adjusted Blade links

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -10,8 +10,8 @@
 
 <header class="container-fluid bg-dark fixed-top c-header">
   <nav class="navbar navbar-expand-lg navbar-dark" role="navigation">
-    <a class="navbar-brand" href="{{ url('/') }}">
-      <img src="{{ url('/img/reclame-ali-white.png') }}" width="30" height="30" class="d-inline-block align-top" alt="Logo do sistema" />
+    <a class="navbar-brand" href="{{ route('home') }}">
+      <img src="{{ asset('img/reclame-ali-white.png') }}" width="30" height="30" class="d-inline-block align-top" alt="Logo do sistema" />
       <span class="text-white-50 h4 c-title">Reclame Ali</span>
     </a>
     <div class="container">
@@ -27,11 +27,12 @@
         </ul>
       @endisset
     </div>
-    <div class="form-inline">
-      <a href="{{ url('/sair') }}" class="btn btn-sm btn-outline-danger text-white my-2 my-sm-0">
+    <form action="{{ route('logout') }}" method="POST" class="form-inline">
+      @csrf
+      <button type="submit" class="btn btn-sm btn-outline-danger text-white my-2 my-sm-0">
         <i class="fas fa-door-open"></i>
         Sair
-      </a>
-    </div>
+      </button>
+    </form>
   </nav>
 </header>
