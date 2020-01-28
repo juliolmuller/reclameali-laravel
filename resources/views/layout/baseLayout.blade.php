aed6f603b1539a37934411ad37ed5b0e2b6c12d8
Julio Muller <juliodolago@gmail.com>
2020-01-28 08:35:39 -0300

    Added dynamic elements to baseLayout.blade

--- resources/views/layout/baseLayout.blade.php
+++ resources/views/layout/baseLayout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -9,7 +9,8 @@
     <title>
       BEIBE :: Reclame Ali - Serviço de Atendimento ao Cliente
     </title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css" />
     @yield('style')
   </head>
   <body>
