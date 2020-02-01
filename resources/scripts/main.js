27f36d02e0f3184347b11e4596685df8d03cd613
Julio Muller <juliodolago@gmail.com>
2020-02-01 11:13:24 -0300

    Added CSRF token to all AJAX requests

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -4,6 +4,15 @@
 import './vendor'
 
 /**
+ * Configure jQuery AJAX to send CSRF Token in every request
+ */
+const token = $('meta[name="csrf-token"]').attr('content')
+if (token)
+  $.ajaxSetup({ headers: { 'X-CSRF-TOKEN': token } })
+else
+  console.error('CSRF token not found')
+
+/**
  * Import custom scripts
  */
 import './scripts'
