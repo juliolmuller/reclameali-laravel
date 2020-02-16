97b93bec311fc326bb4e9778d47067adea5b0773
Julio Muller <juliodolago@gmail.com>
2020-02-16 20:14:12 -0300

    Added Vue.js to the project

--- resources/scripts/vendor.js
+++ resources/scripts/vendor.js
@@ -1,18 +1,23 @@
 
-/**
+/*
  * Import Bootstrap dependencies
  */
 window.$ = window.jQuery = require('jquery')
 window.popper = require('popper.js').default
 import 'bootstrap'
 
-/**
+/*
  * Import jQuery plugins
  */
 import 'jquery-mask-plugin'
 import '@chenfengyuan/datepicker'
 
-/**
+/*
  * Import toastr
  */
 window.toastr = require('toastr')
+
+/*
+ * Import Vue.js framework
+ */
+window.Vue = require('vue')
