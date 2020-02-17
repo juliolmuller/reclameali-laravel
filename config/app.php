1f29e1e5d1289bd9e2e3d9d88fa3c61abaeb1c08
Julio Muller <juliodolago@gmail.com>
2020-02-16 23:24:38 -0300

    Added package 'active' for Laravel

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\ValidationServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Watson\Active\ActiveServiceProvider::class,
 
     ],
 
